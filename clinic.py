1b59726da7401eacc35d9b73b1a801f4b4f112b5
Mohammad Atieh <mohammad.attieh@gmail.com>
2022-05-06 10:17:23 +0300

    commit

--- clinic.py
+++ clinic.py
@@ -1,11 +1,17 @@
 import datetime
+import os
+import csv
 import test_clinic
+from test_clinic import *
+
 
 ### Global Variables ###
 
 patients = []
 doctors = []
 nurses = []
+receptionists = []
+
 prescriptions = []
 appointments = []
 
@@ -110,10 +116,167 @@ class AppointmentSchedule():
 
 
 
+# selected_receptionist = None
+# selected_nurse = None
+# selected_doctor= None
+
+
+### Functions ###
+
+def main_screen():
+    '''Main user interface'''
+
+    main_menu = input("\033[96mPlease select user:\033[0m\n\n\
+        \033[93m 1:\033[0m Receptionist\n\
+        \033[93m 2:\033[0m Nurse \n\
+        \033[93m 3:\033[0m Doctor\n\
+        \033[93m-1:\033[0m Anywhere to return to the main menu \n\
+        \033[93m 0:\033[0m Quit\n\n\
+        > ")
+
+    if main_menu == '1':
+                
+        pass
+
+       
+       
+    # elif main_menu == '2':
+    #     clear_screen()
+    #     add_patient_interface()
+    #     export_to_cv()
+    #     clear_screen()
+    #     main_screen()
+
+    # elif main_menu == '3':
+    #     clear_screen()
+    #     add_test_patients()
+    #     export_to_cv() 
+    #     print('') # A new line
+    #     go_to_main_screen = input(
+    #         '\033[96mHit enter to view schedule or enter -1 to go to main menu\033[0m: ')
+
+    #     if go_to_main_screen == '':
+    #         clear_screen()
+    #         print_follow_up_schedule()
+    #         print('') # A new line
+    #         go_to_main_screen = input(
+    #             '\033[96mHit enter to return to main screen\033[0m: ')
+    #         if go_to_main_screen.isascii():
+    #             clear_screen()
+    #             main_screen()
+    #     else:
+    #         clear_screen()
+    #         main_screen()
+
+    # elif main_menu == '4':
+    #     edit_delete_interface()
+    #     export_to_cv()
+    #     clear_screen()
+    #     main_screen()
+
+    # elif main_menu == '5':
+    #     clear_screen()
+    #     print_follow_up_schedule()
+    #     print('') # A new line
+    #     sort_interface()
+
+    # elif main_menu in ['0', '-1']:
+    #     clear_screen()
+    #     print('\033[92mGood Bye!\033[0m')
+    #     quit()
+
+    # elif main_menu == '6':
+    #     clear_screen()
+    #     print_follow_up_schedule()
+    #     print('') # A new line
+    #     check_out_and_follow_interface()
+
+    else:
+        
+        go_to_main_screen = input('\033[96mPlease enter one of the mentioned options only. Hit enter to try again, otherwise the application will quit\033[0m ')
+
+        if go_to_main_screen == "":
+        
+            clear_screen()
+            main_menu = ''
+            main_screen()
+
+        else:
+            quit()
+
+
+def add_object_to_list(user,users_list):
+    '''Saves user data to a users list.
+    for example, saves a doctor information to doctors list
+    '''
+    if not users_list:
+        user_list_header = (list(vars(user).keys()))
+        users_list.append(user_list_header)
+        user_info = (list(vars(user).values()))
+        users_list.append(user_info)
+
+    else:
+        user_info = (list(vars(user).values()))
+        users_list.append(user_info)
+  
+    return users_list
+
+
+def list_to_csv(users_list,file):
+    '''Converts users data from a list to a csv file'''
+
+    file = os.getcwd() + f'/{file}.csv'
+
+    with open(file, 'w') as f:
+        writer = csv.writer(f)
+        for row in users_list:
+            writer.writerow(row)
+
+    return os.path.basename(file)[:-4]
+
+
+def csv_to_list(users_list,file):
+    '''Converts users data from a csv file to a list'''
+
+    file = os.getcwd() + f'/{file}.csv'
+    with open(file, 'r') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            if row:
+                users_list.append(row)
+
+
+def list_to_object(user_type, user_info):
+    '''Converts one user data from a list to an object'''
+    
+    user_object = user_type(*user_info)
+    return user_object
+
+
+def clear_screen():
+    '''Clears the command line interface.'''
+
+    return os.system(
+        'cls' if os.name in ('nt', 'dos') else 'clear')
+
+
+
+
+##### DATA IMPORT & CLEAR SCREEN #####
+
+# import_from_cv()
+
+
+
+
+
+##### User Interface #####
+
+if __name__ == '__main__':
+    test_clinic.unittest.main(exit=False)
+    
+    main_screen()
 
     
-test_clinic.unittest.main()
-
-   
 
     
--- test_clinic.py
+++ test_clinic.py
@@ -1,37 +1,89 @@
+from pydoc import doc
 import unittest
+import clinic
 from clinic import *
+
+
+
 
 class TestClinic(unittest.TestCase):
 
-    # @classmethod
-    # def setUpClass(cls) -> None:
+
+    @classmethod
+    def setUpClass(cls) -> None:
+    
+        print('\033[38;5;15;48;5;20m Running Self Tests!:\033[0m')
+
+     
+        cls.users_list = []
+        cls.patient_1 = Patient('Rolland Manassa','3844 Yeager St','712-235-9932')
+        cls.patient_info = ['Rolland Manassa', '3844 Yeager St', '712-235-9932']
+        cls.users_list_1 =[['name', 'address', 'phone'], ['Rolland Manassa', '3844 Yeager St', '712-235-9932']]
+        cls.users_list_2=[['name', 'address', 'phone'], ['Rolland Manassa', '3844 Yeager St', '712-235-9932'],
+        ['Jude Basin','8069 Florian St','275-850-5092']]
+        cls.patient_2 = Patient('Jude Basin','8069 Florian St','275-850-5092')
+
+        cls.healthcare_professional_info= ['David Miller','1']
+
+        
+
+    def test_save_data_to_list(self):
+        '''Test saving user data to a list'''
+
        
-    #     print('''\n\033[38;5;15;48;5;20mTesting\033[0m
 
-    #     --------------------------------------------
-
-    #     ''')
-
-    # @classmethod
-    # def tearDownClass(cls) -> None:
-    #    pass
+        # Test if the first user information is appended to the list correctly
+    
+        saved_data = add_object_to_list(self.patient_1,self.users_list)
+        self.assertEqual(saved_data,self.users_list_1)
 
 
-    def test_first(self):
-        print('First false test has passed')
+        # Test if subsequent users are added to the list correctly without duplicating the header
+        
+        saved_data = add_object_to_list(self.patient_2,self.users_list)
+        self.assertEqual(saved_data,self.users_list_2)
 
-        self.assertEqual(1,1)
+
+    def test_list_to_object(self):
+        '''Test converting user info to an object'''
+
+
+        # Test converting patient info to an object
+        patient = list_to_object(Patient,self.patient_info)
+        self.assertIsInstance(patient,Patient)
+
+        # Test converting healthcare professional info to an object
+        healthcare_professional = list_to_object(Doctor,self.healthcare_professional_info)
+        self.assertIsInstance(healthcare_professional,HealthCareProfessional)
+
+
+
+    def test_save_to_csv(self):
+        '''Test saving users list to csv file and loading form the file back to the list'''
+
+        file = list_to_csv(self.users_list_2,'patients')
+        users_list=[]
+        csv_to_list(users_list,file)
+        self.assertEqual(users_list,self.users_list_2)
+
+
+
 
 
 receptionist_1 = Receptionist('Susan','1')
- 
+
 doctor_1 = Doctor("Mohammad Atieh",1)
 
-patient_1 = Patient('Rami','Amman','5232208')
+patient_tuple = 'Rami','Amman','5232208'
+patient_1 = Patient(*patient_tuple)
 
 appointment_1 = patient_1.request_appointment(receptionist_1,doctor_1)
 
-print(appointment_1)
+print(vars(patient_1).values())
+
+
+
+
 
 
 
