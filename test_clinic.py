543ea0a1a027b15c1990dbcfeee4c70e3b358660
Mohammad Atieh <mohammad.attieh@gmail.com>
2022-05-11 08:17:18 +0300

    commit

--- test_clinic.py
+++ test_clinic.py
@@ -1,4 +1,3 @@
-from pydoc import doc
 import unittest
 import clinic
 from clinic import *
@@ -16,56 +15,63 @@ class TestClinic(unittest.TestCase):
 
      
         cls.users_list = []
-        cls.patient_1 = Patient('Rolland', 'Manassa','3844 Yeager St','712-235-9932')
+        cls.patient_1 = Patient('Rolland', 'Manassa','3844 Yeager St','712-235-9932','1')
         cls.patient_info = ['Rolland', 'Manassa', '3844 Yeager St', '712-235-9932','1']
-        cls.users_list_1 =[['File No.','First Name','Last Name', 'address', 'phone'], ['1','Rolland', 'Manassa', '3844 Yeager St', '712-235-9932']]
-        cls.users_list_2=[['File No.','First Name','Last Name','address', 'phone'], ['1','Rolland', 'Manassa', '3844 Yeager St', '712-235-9932'],
-        ['Jude Basin','8069 Florian St','275-850-5092']]
+        cls.patients_list_1 =[['Rolland', 'Manassa', '3844 Yeager St', '712-235-9932','1']]
+        cls.patients_list_2=[['Rolland', 'Manassa', '3844 Yeager St', '712-235-9932','1'],
+        ['Jude', 'Basin','8069 Florian St','275-850-5092','2']]
         cls.patient_2 = Patient('Jude','Basin','8069 Florian St','275-850-5092','2')
 
         cls.healthcare_professional_info= ['David Miller','1']
 
+
+    def test_register_patient(self):
+        '''Tests the process of registering a patient'''
+
+        new_patient_1 = self.patient_1
+        new_patient_1.register()
+        new_patient_2 = self.patient_1
+        new_patient_2.register()
+        print('here',new_patient_1)
+
+
+        self.assertEqual(Patient.patients_list, [new_patient_1])
         
 
     def test_save_data_to_list(self):
-        '''Test saving user data to a list'''
+        '''Test saving object data to a list'''
 
-       
+        saved_data = objects_to_list([self.patient_1])
+        self.assertEqual(saved_data,self.patients_list_1)
 
-        # Test if the first user information is appended to the list correctly
-    
-        saved_data = add_object_to_list(self.patient_1,self.users_list)
-        self.assertEqual(saved_data,self.users_list_1)
-
-
-        # Test if subsequent users are added to the list correctly without duplicating the header
-        
-        saved_data = add_object_to_list(self.patient_2,self.users_list)
-        self.assertEqual(saved_data,self.users_list_2)
+        saved_data = objects_to_list([self.patient_1,self.patient_2])
+        self.assertEqual(saved_data,self.patients_list_2)
 
 
     def test_list_to_object(self):
-        '''Test converting user info to an object'''
-
+        '''Test converting object data from list to an object'''
 
         # Test converting patient info to an object
         patient = list_to_object(Patient,self.patient_info)
         self.assertIsInstance(patient,Patient)
+        self.assertEqual(patient.first_name,'Rolland')
+        self.assertEqual(patient.last_name,'Manassa')
+        self.assertEqual(patient.address,'3844 Yeager St')
+        self.assertEqual(patient.phone,'712-235-9932')
+        self.assertEqual(patient.file_no,'1')
 
         # Test converting healthcare professional info to an object
         healthcare_professional = list_to_object(Doctor,self.healthcare_professional_info)
         self.assertIsInstance(healthcare_professional,HealthCareProfessional)
 
 
-
     def test_save_to_csv(self):
         '''Test saving users list to csv file and loading form the file back to the list'''
 
-        file = list_to_csv(self.users_list_2,'patients')
-        users_list=[]
-        csv_to_list(users_list,file)
-        self.assertEqual(users_list,self.users_list_2)
-
+        file = list_to_csv(self.patients_list_2,'patients_list_test')
+        patients_list = []
+        csv_to_list(patients_list,file)
+        self.assertEqual(patients_list,self.patients_list_2)
 
 
 
